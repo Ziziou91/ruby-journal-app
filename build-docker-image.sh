fe04f80d5130ca64b36d8f84f741fb2202a664f5
Joshua Gray <joshgray91@gmail.com>
2025-04-02 11:43:46 +0100

    remove ruby_script.rb

--- build-docker-image.sh
+++ build-docker-image.sh
@@ -1,7 +1,15 @@
 #!/bin/bash
+until ! lsof -i :3000; do
+  docker kill  $(docker ps -q --filter "expose=3000")
+
+  echo "Waiting for port 3000 to be free..."
+  sleep 1
+done
 
 # Build journal-app docker image
 docker build -t journal-app:latest .
 
 # Run journal-app docker image
 CONTAINER_ID=$(docker run -e RAILS_ENV=development -p 3000:3000 -dit journal-app:latest)
+
+echo "New container started with ID: $CONTAINER_ID"
