e896c3915478eea407e9be0c3e8f2174efe8d182
Joshua Gray <joshgray91@gmail.com>
2025-04-07 17:29:52 +0100

    implement initial calendar

--- journal-app/config/application.rb
+++ journal-app/config/application.rb
@@ -1,6 +1,5 @@
 require_relative "boot"
 require "rails/all"
-require_relative "../lib/middleware/force_trailing_slash"
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
@@ -8,8 +7,6 @@ Bundler.require(*Rails.groups)
 
 module JournalApp
   class Application < Rails::Application
-    config.middleware.use ForceTrailingSlash
-
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.2
 
