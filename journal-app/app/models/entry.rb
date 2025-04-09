7f65d6fe2af7ded78dc7768598009903c104595f
Joshua Gray <joshgray91@gmail.com>
2025-04-09 16:13:23 +0100

    implement share entry functionality

--- journal-app/app/models/entry.rb
+++ journal-app/app/models/entry.rb
@@ -1,5 +1,5 @@
 class Entry < ApplicationRecord
-    belongs_to :user # The user who created the entry
+    belongs_to :user
     has_many :shared_entries, dependent: :destroy
     has_many :shared_with_users, through: :shared_entries, source: :user
 
